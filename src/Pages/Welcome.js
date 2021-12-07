7c9f15ef1c1660990a0a72239a1f0ac0f5bdf037
faldibule <faldibule@gmail.com>
2021-12-07 23:45:27 +0700

    coba coba content loader, tp baru nyoba doang

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import ContentLoader from 'react-content-loader'
 
 const Welcome = () => {
     return (
         <div>
             <p>Halaman Welcome </p>
-            <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                <p>
-                    <Skeleton count={3} />
-                </p>
-            </SkeletonTheme>
+            <ContentLoader 
+                speed={2}
+                width={400}
+                height={160}
+                viewBox="0 0 400 160"
+                backgroundColor="#adb5bd"
+                foregroundColor="#ced4da"
+            >
+                <rect x="48" y="8" rx="3" ry="3" width="88" height="6" /> 
+                <rect x="48" y="26" rx="3" ry="3" width="52" height="6" /> 
+                <circle cx="20" cy="20" r="20" />
+            </ContentLoader>
         </div>
     )
 }
