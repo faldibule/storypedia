58dd2bd786135cc6984953cd4997af495fa1c143
faldibule <faldibule@gmail.com>
2021-12-16 22:18:48 +0700

    menyelesaikan card section

--- src/Component/Reuse/CardWelcome.js
+++ src/Component/Reuse/CardWelcome.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const CardWelcome = ({title, body}) => {
+const CardWelcome = ({title, body, icon}) => {
+
     return (
         <div className="col-md-3 d-flex align-items-start">
             <div>
-                <h2><span className='text-warning'>{title.substring(0, 1)}</span>{title.substr(1, title.length)}</h2>
+                <h2>{icon} <span className='text-warning'>{title.substring(0, 1)}</span>{title.substr(1, title.length)}</h2>
                 <p>{body}</p>
             </div>
         </div>
--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Parallax, Background } from 'react-parallax'
+import { Flower3, NodePlusFill, PieChartFill, Truck } from "react-bootstrap-icons";
 import CardWelcome from '../Component/Reuse/CardWelcome'
 import ParallaxComponent from '../Component/Reuse/ParallaxComponent'
 import JumboImg from '../Images/other/2.jpg'
@@ -28,24 +28,38 @@ const Welcome = () => {
             </div>
             <br />
             <div className="container px-4 py-5" id="hanging-icons">
-                <img className='img-fluid' style={{width: '100%', height: '300px', objectFit: 'cover', objectPosition: 'center'}} src={MernImg} alt="mern Stack" />
+                <h1 className='text-center'>
+                    <span className='text-success'>M </span> 
+                    <span className='text-secondary'>E</span> 
+                    <span className='text-primary'> R </span> 
+                    <span className='text-success'>N </span> 
+                    <span className='text-warning'>    S T A C K</span>
+
+                </h1>
+                
                 <hr />
                 <div className="row">
-                    <CardWelcome 
+                    <CardWelcome
+                        icon={<img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="" />} 
                         title="MongoDB" 
                         body="MongoDB adalah salah satu jenis database NoSQL yang cukup populer digunakan dalam pengembangan website. Berbeda dengan database jenis SQL yang menyimpan data menggunakan relasi tabel, MongoDB menggunakan dokumen dengan format JSON. 
                         Hal inilah yang dianggap membuat pengelolaan data menggunakan MongoDB lebih baik. Alhasil, banyak perusahaan besar seperti Adobe, Google dan ebay yang menggunakannya. " 
                     />
-                    <CardWelcome 
+                    <CardWelcome
+                        icon={<img src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg
+                        " alt="" />} 
                         title="ExpressJS" 
                         body="Express JS adalah framework dari NodeJS yang dirancang secara fleksibel dan sederhana untuk membantu tahap pengembangan aplikasi back end. Express JS juga sangat berbeda dengan framework Laravel, dimana library ini memberikan kebebasan bagi para developer untuk mendesain aplikasi, sehingga memungkinan bagi setiap pengembang memiliki rancangan arsitektur yang berbeda dalam software yang dibangun" 
                     />
-                    <CardWelcome 
+                    <CardWelcome
+                        icon={<img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg
+                        " alt="" />}  
                         title="ReactJS" 
                         body="ReactJS adalah JavaScript Library yang dikembangkan oleh Jordan Walke sekitar tahun 2013. Saat itu, Walke merupakan seorang developer untuk Facebook. Saat ini, ReactJS digunakan sebagai salah satu framework untuk membuat bagian front-end dari sebuah aplikasi. Dilansir dari laman resminya, ReactJS diklaim membantumu membuat UI interaktif dengan mudah.
                         ReactJS akan secara efisien memperbarui dan merender komponen yang tepat saat datamu berubah."
                     />
-                    <CardWelcome 
+                    <CardWelcome
+                        icon={<img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg" alt="" />}   
                         title="NodeJS" 
                         body="Node.js adalah runtime environment untuk JavaScript yang bersifat open-source dan cross-platform. Dengan Node.js kita dapat menjalankan kode JavaScript di mana pun, tidak hanya terbatas pada lingkungan browser.Node.js menjalankan V8 JavaScript engine (yang juga merupakan inti dari Google Chrome) di luar browser. Ini memungkinkan Node.js memiliki performa yang tinggi." 
                     />
