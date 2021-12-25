101a470606f039370edc6c05b9aa6c9841636558
faldibule <faldibule@gmail.com>
2021-12-25 12:04:07 +0700

    menambah marquee pada mern section

--- src/Component/Welcome/CardWelcome.js
+++ src/Component/Welcome/CardWelcome.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Card } from "react-bootstrap";
 
 const CardWelcome = ({title, body, icon}) => {
 
     return (
-        <div className="col-md-3 d-flex align-items-start">
-            <div>
-                <h2>{icon} <span className='text-warning'>{title.substring(0, 1)}</span>{title.substr(1, title.length)}</h2>
-                <p>{body}</p>
-            </div>
-        </div>
+        <Card style={{ width: '20rem', height: '50vh' }} className="mx-3">
+            <Card.Header>
+                <h2 align="center">{icon} <span className='text-warning'>{title.substring(0, 1)}</span>{title.substr(1, title.length)}</h2>
+            </Card.Header>
+            <Card.Body>
+                <Card.Text className="text-center">
+                    {body}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
---
+++ src/Component/Welcome/MarqueeComponent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+import CardWelcome from './CardWelcome'
+
+const MarqueeComponent = ({speed}) => {
+    const marqueeStyle = {display: 'flex', alignItems: 'top'}
+    return (
+        <Marquee style={marqueeStyle} pauseOnHover={true} gradient={false} speed={speed}>
+                <CardWelcome
+                    icon={<img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="" />} 
+                    title="MongoDB" 
+                    body="MongoDB adalah salah satu jenis database NoSQL yang cukup populer digunakan dalam pengembangan website. Berbeda dengan database jenis SQL yang menyimpan data menggunakan relasi tabel, MongoDB menggunakan dokumen dengan format JSON." 
+                /> 
+                <CardWelcome
+                    icon={<img src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg
+                    " alt="" />} 
+                    title="ExpressJS" 
+                    body="Express JS adalah framework dari NodeJS yang dirancang secara fleksibel dan sederhana untuk membantu tahap pengembangan aplikasi back end. " 
+                />
+                <CardWelcome
+                    icon={<img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg
+                    " alt="" />}  
+                    title="ReactJS" 
+                    body="ReactJS adalah JavaScript Library yang dikembangkan oleh Jordan Walke sekitar tahun 2013. Saat itu, Walke merupakan seorang developer untuk Facebook. Saat ini, ReactJS digunakan sebagai salah satu framework untuk membuat bagian front-end dari sebuah aplikasi. "
+                />
+                <CardWelcome
+                    icon={<img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg" alt="" />}   
+                    title="NodeJS" 
+                    body="Node.js adalah runtime environment untuk JavaScript yang bersifat open-source dan cross-platform. Dengan Node.js kita dapat menjalankan kode JavaScript di mana pun, tidak hanya terbatas pada lingkungan browser." 
+                />
+            </Marquee>
+    )
+}
+
+export default MarqueeComponent
